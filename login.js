3ffd09a19215e1a1eaed7448ce162f90c34b535b
Mohammed saif <saifmohammed888@gmail.com>
2023-05-06 00:14:32 +0530

    web app changes

---
+++ login.js
@@ -0,0 +1,42 @@
+// Dummy credentials
+const dummyUsername = "M Suhaib";
+const dummyPassword = "pass123";
+
+// Get form and inputs
+const form = document.querySelector('form');
+const usernameInput = document.querySelector('#username');
+const passwordInput = document.querySelector('#password');
+
+
+let btn = document.getElementById("loginBtn");
+let logoutBtn =document.getElementById("logoutBtn");
+let userName = document.getElementById("userNames")
+
+// Check if user is already logged in
+if (localStorage.getItem('loggedIn') === 'true') {
+    btn.style.display = "none"
+    logoutBtn.style.display = "flex"
+    userName.innerText = localStorage.getItem("user")
+}else{
+    logoutBtn.style.display = "none"
+    btn.style.display = "block"
+}
+// Listen for form submit event
+form.addEventListener('submit', (event) => {
+	event.preventDefault(); // Prevent form from submitting
+
+	// Get input values
+	const username = usernameInput.value;
+	const password = passwordInput.value;
+
+	// Check if credentials match
+	if (username === dummyUsername && password === dummyPassword) {
+		// Store login status in local storage
+		localStorage.setItem('loggedIn', 'true');
+		localStorage.setItem('user', 'M Suhaib');
+		window.location.href = 'doctors.html';
+	} else {
+		alert('Invalid username or password');
+	}
+});
+
